a6fdbcbc441bc325ae59c140f4050bd721a6f8c1
Lethiciahas <lethiciahas@gmail.com>
2022-10-29 09:31:36 -0400

    feat:criado test para o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    expected = Product(
+        "10", "Titanium Dioxide", "Target Corporation", "2020-12-08",
+        "2023-12-08", "FR29 5791 5333 58XR G4PR IG28 D08", "instrucao 10")
+    report = str(
+        f"O produto {expected.nome_do_produto}"
+        f" fabricado em {expected.data_de_fabricacao}"
+        f" por {expected.nome_da_empresa} com validade"
+        f" até {expected.data_de_validade}"
+        f" precisa ser armazenado {expected.instrucoes_de_armazenamento}."
+    )
+
+    assert expected.__repr__() == report
