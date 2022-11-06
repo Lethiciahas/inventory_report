df1c71c236121da83976af54af292ad70788bd94
Lethiciahas <lethiciahas@gmail.com>
2022-11-06 08:48:54 -0400

    feat: criado metodos para gerar relatorios csv e json

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,28 @@
+import csv
+import json
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+def import_csv(path, type):
+    with open(path) as file:
+        reader = csv.DictReader(file)
+        _list = list(reader)
+        if type == "simples":
+            return SimpleReport.generate(_list)
+        return CompleteReport.generate(_list)
+
+
+def import_json(path, type):
+    with open(path) as file:
+        _list = json.load(file)
+        if type == "simples":
+            return SimpleReport.generate(_list)
+        return CompleteReport.generate(_list)
+
+
+class Inventory():
+    def import_data(path, type):
+        if ".csv" in path:
+            return import_csv(path, type)
+        return import_json(path, type)
