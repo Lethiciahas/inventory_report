bfdcd1e363f8af0bdf224dcc2b350f8ecc18a39a
Lethiciahas <lethiciahas@gmail.com>
2022-11-06 10:22:09 -0400

    feat: testes

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,29 +1,29 @@
 import csv
 import json
-import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
-class Inventory:
-    @classmethod
-    def import_data(cls, path, type):
-        data = cls.reports(path)
+def import_csv(path, type):
+    with open(path) as file:
+        reader = csv.DictReader(file)
+        _list = list(reader)
         if type == "simples":
-            return SimpleReport.generate(data)
-        elif type == "completo":
-            return CompleteReport.generate(data)
-        return TypeError("typeNotFound")
+            return SimpleReport.generate(_list)
+        return CompleteReport.generate(_list)
 
-    @classmethod
-    def reports(cls, path):
-        if path.endswith('csv'):
-            with open(path) as file:
-                _list = list(csv.DictReader(file))
-            return _list
-        elif path.endswith('json'):
-            with open(path) as file:
-                return json.loads(file.read())
-        elif path.endswith('xml'):
-            with open(path) as file:
-                return xmltodict.parse(file.read())["dataset"]["record"]
+
+def import_json(path, type):
+    with open(path) as file:
+        _list = json.load(file)
+        if type == "simples":
+            return SimpleReport.generate(_list)
+        return CompleteReport.generate(_list)
+
+
+class Inventory():
+    def import_data(path, type):
+        if ".csv" in path:
+            return import_csv(path, type)
+        elif ".json" in path:
+            return import_json(path, type)
