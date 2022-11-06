16b59eadd7b29b29aaf772671b479cc701b87690
Lethiciahas <lethiciahas@gmail.com>
2022-11-06 09:14:16 -0400

    feat: criado metodos para gerar relatorios xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -7,22 +8,33 @@ from inventory_report.reports.complete_report import CompleteReport
 def import_csv(path, type):
     with open(path) as file:
         reader = csv.DictReader(file)
-        _list = list(reader)
+        data = list(reader)
         if type == "simples":
-            return SimpleReport.generate(_list)
-        return CompleteReport.generate(_list)
+            return SimpleReport.generate(data)
+        return CompleteReport.generate(data)
 
 
 def import_json(path, type):
     with open(path) as file:
-        _list = json.load(file)
+        data = json.load(file)
         if type == "simples":
-            return SimpleReport.generate(_list)
-        return CompleteReport.generate(_list)
+            return SimpleReport.generate(data)
+        return CompleteReport.generate(data)
+
+
+def import_xml(path, type):
+    with open(path) as file:
+        data = xmltodict.parse(file.read())["dataset"]["record"]
+        if type == "simples":
+            return SimpleReport.generate(data)
+        return CompleteReport.generate(data)
 
 
 class Inventory():
     def import_data(path, type):
         if ".csv" in path:
             return import_csv(path, type)
-        return import_json(path, type)
+        elif ".json" in path:
+            return import_json(path, type)
+        else:
+            return import_xml(path, type)
